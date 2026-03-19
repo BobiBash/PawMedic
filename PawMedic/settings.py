91fcb7b5258b38bde12f277f28266eac48adf34d
hoppa7 <bobgon050@gmail.com>
2026-03-19 15:56:33 +0200

    move secret key to env vars

--- PawMedic/settings.py
+++ PawMedic/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^mj#^tl&9&pfsi%dl=6-8on40!v0d3e3iqopgjz+q=8m)*wi=p'
+SECRET_KEY = f'django-insecure-{os.getenv('SECRET_KEY')}'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
