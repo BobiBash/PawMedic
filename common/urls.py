0716da9ec30613bb6ea0ba9fa92453361cdf84bf
Bobi . <bobgon050@gmail.com>
2026-04-07 01:20:31 +0300

    update navbar

--- common/urls.py
+++ common/urls.py
@@ -3,4 +3,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='common/home.html'), name='home'),
+    path('about/', TemplateView.as_view(template_name='common/about.html'), name='about')
 ]
